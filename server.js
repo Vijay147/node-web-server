3a069e15880d2bd4cc0ccaafdae7a6557d118b01
AzureAD\VishnuVijay <vishnu.vijay@ibm.com>
2018-08-07 23:20:48 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -56,6 +57,6 @@ app.get('/info', (req, res) => {
     });
 });
 
-app.listen(8097, () => {
-    console.log('Server is up and running on port 8097');
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
 });
